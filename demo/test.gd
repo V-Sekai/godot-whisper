92cd2422b25171ff812ec9a3f4e2046eea9c1535
Dragos Daian <daian.dragos@yahoo.com>
2023-12-02 22:15:22 +0100

    fix warnings

--- demo/test.gd
+++ demo/test.gd
@@ -16,8 +16,8 @@ func _ready():
 	speech_to_text.duration_ms = int(interval * 1000)
 
 func merge_with_old_tokens(new_tokens: Array):
-	var offset : int = (interval - keep_interval / 2) * 100
-	var offset_reverse : int = keep_interval / 2 * 100
+	var offset := int((interval - keep_interval / 2) * 100)
+	var offset_reverse := int(keep_interval / 2 * 100)
 	var last_word_deleted := ""
 	for i in range(tokens.size()-1, -1, -1):
 		# remove from here as they are positive
@@ -37,14 +37,14 @@ func merge_with_old_tokens(new_tokens: Array):
 	tokens.append_array(new_tokens)
 	
 
-func tokens_to_text(tokens):
+func tokens_to_text(speech_tokens):
 	text = ""
-	for token in tokens:
-		text += token["text"]
+	for token in speech_tokens:
+		text += speech_tokens["text"]
 	print("Text: ... ", text, " ...")
 
-func _thread_function(buffer_full):
-	var new_tokens :Array= speech_to_text.transcribe(buffer_full)
+func _thread_function(buffer):
+	var new_tokens :Array= speech_to_text.transcribe(buffer)
 	new_tokens = new_tokens.filter(func (token): return !("[" in token["text"]) && !("<" in token["text"]))
 	if !tokens.is_empty():
 		merge_with_old_tokens(new_tokens)
@@ -56,8 +56,8 @@ func _process(_delta):
 	var buffer: PackedVector2Array = effect_capture.get_buffer(effect_capture.get_frames_available())
 	buffer_full.append_array(buffer)
 	var mix_rate : int = ProjectSettings.get_setting("audio/driver/mix_rate")
-	var total_len : int = mix_rate * interval
-	var keep_len : int = mix_rate * keep_interval
+	var total_len := int(mix_rate * interval)
+	var keep_len := int(mix_rate * keep_interval)
 	if buffer_full.size() > total_len:
 		buffer_full.slice(buffer_full.size() - total_len)
 		var thread = Thread.new()
