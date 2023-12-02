0300c03bb69a67b77b7d821cab952f4cffe6f64d
Dragos Daian <daian.dragos@yahoo.com>
2023-12-02 20:33:17 +0100

    Reenable android and win build (#13)
    * Update runner.yml
    
    * remove models.
    
    * update
    
    * Update speech_to_text.cpp
    
    * update

--- demo/test.gd
+++ demo/test.gd
@@ -1,24 +1,66 @@
 extends Node2D
 
+@export var interval := 5.0
+@export var keep_interval := 2.0
+@export var text := ""
+@export var tokens: Array
 @onready var idx = AudioServer.get_bus_index("Record")
 @onready var effect_capture := AudioServer.get_bus_effect(idx, 0) as AudioEffectCapture
 @onready var speech_to_text : SpeechToText = $SpeechToText
-@onready var audio_stream_player :AudioStreamPlayer
 var buffer_full : PackedVector2Array
+
+func _ready():
+	if effect_capture.buffer_length < interval:
+		push_warning("buffer_length smaller than interval.")
+		interval = effect_capture.buffer_length
+	speech_to_text.duration_ms = int(interval * 1000)
+
+func merge_with_old_tokens(new_tokens: Array):
+	var offset : int = (interval - keep_interval / 2) * 100
+	var offset_reverse : int = keep_interval / 2 * 100
+	var last_word_deleted := ""
+	for i in range(tokens.size()-1, -1, -1):
+		# remove from here as they are positive
+		if tokens[i]["t0"] - offset < 0:
+			tokens = tokens.slice(0, i + 1)
+			last_word_deleted = tokens[i]["text"]
+			break
+	tokens_to_text(tokens)
+	tokens_to_text(new_tokens)
+	print(last_word_deleted)
+	for i in range(new_tokens.size()):
+		# remove from here as they are positive
+		if new_tokens[i]["t0"] - offset_reverse > 0:
+			new_tokens = new_tokens.slice(i)
+			break
+	tokens_to_text(new_tokens)
+	tokens.append_array(new_tokens)
+	
+
+func tokens_to_text(tokens):
+	text = ""
+	for token in tokens:
+		text += token["text"]
+	print("Text: ... ", text, " ...")
+
+func _thread_function(buffer_full):
+	var new_tokens :Array= speech_to_text.transcribe(buffer_full)
+	new_tokens = new_tokens.filter(func (token): return !("[" in token["text"]) && !("<" in token["text"]))
+	if !tokens.is_empty():
+		merge_with_old_tokens(new_tokens)
+	else:
+		tokens = new_tokens
+	tokens_to_text(tokens)
 
 func _process(_delta):
 	var buffer: PackedVector2Array = effect_capture.get_buffer(effect_capture.get_frames_available())
 	buffer_full.append_array(buffer)
-	#buffer_full.resize(speech_to_text.step_ms)
-	#print(effect_transcribe.is_transcribing_active())
+	var mix_rate : int = ProjectSettings.get_setting("audio/driver/mix_rate")
+	var total_len : int = mix_rate * interval
+	var keep_len : int = mix_rate * keep_interval
+	if buffer_full.size() > total_len:
+		buffer_full.slice(buffer_full.size() - total_len)
+		var thread = Thread.new()
+		thread.start(_thread_function.bind(buffer_full.duplicate()))
+		buffer_full = buffer_full.slice(buffer_full.size() - keep_len, buffer_full.size())
 
-func ellapsed(time: float):
-	return Time.get_unix_time_from_system() - time
-
-func transcribe():
-	var start = Time.get_unix_time_from_system()
-	buffer_full.resize(ProjectSettings.get_setting("audio/driver/mix_rate") * effect_capture.buffer_length)
-	print(ellapsed(start))
-	print("text " + speech_to_text.transcribe(buffer_full))
-	print(ellapsed(start))
-	buffer_full.clear()
--- src/speech_to_text.h
+++ src/speech_to_text.h
@@ -5,6 +5,7 @@
 #include <godot_cpp/classes/os.hpp>
 #include <godot_cpp/classes/thread.hpp>
 #include <godot_cpp/templates/vector.hpp>
+#include <godot_cpp/variant/array.hpp>
 
 #include <libsamplerate/src/samplerate.h>
 #include <whisper.cpp/whisper.h>
@@ -41,7 +42,7 @@ class SpeechToText : public Node {
 	whisper_context_params context_parameters{ true };
 	Vector<whisper_token> prompt_tokens;
 	whisper_context *context_instance = nullptr;
-
+	int buffer_len;
 	float *buffer_float;
 	float *resampled_float;
 
@@ -52,12 +53,12 @@ public:
 	enum {
 		SPEECH_SETTING_SAMPLE_RATE = 16000,
 	};
-	String transcribe(PackedVector2Array buffer);
+	Array transcribe(PackedVector2Array buffer);
 	_FORCE_INLINE_ void set_language(String p_language) { params.language = p_language.utf8().get_data(); }
 	_FORCE_INLINE_ String get_language() { return String(params.language.c_str()); }
-	_FORCE_INLINE_ void set_language_model(String p_model) { params.model = p_model.utf8().get_data(); }
+	void set_language_model(String p_model);
 	_FORCE_INLINE_ String get_language_model() { return String(params.model.c_str()); }
-	_FORCE_INLINE_ void set_duration_ms(int32_t duration_ms) { params.duration_ms = duration_ms; }
+	void set_duration_ms(int32_t duration_ms);
 	_FORCE_INLINE_ int32_t get_duration_ms() { return params.duration_ms; }
 	_FORCE_INLINE_ void set_use_gpu(bool use_gpu) { context_parameters.use_gpu = use_gpu; }
 	_FORCE_INLINE_ bool is_use_gpu() { return context_parameters.use_gpu; }
