6fbffe02a4e81b4baf6b9a7245cb2f3673c6e1b0
Dragos Daian <daian.dragos@yahoo.com>
2023-12-02 22:27:41 +0100

    Add double build (#14)
    * add double build also
    
    * Update godot_whisper.gdextension
    
    * Update runner.yml
    
    * Update runner.yml

--- demo/test.gd
+++ demo/test.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+signal text_updated(text: String)
+
 @export var interval := 5.0
 @export var keep_interval := 2.0
 @export var text := ""
@@ -16,8 +18,7 @@ func _ready():
 	speech_to_text.duration_ms = int(interval * 1000)
 
 func merge_with_old_tokens(new_tokens: Array):
-	var offset : int = (interval - keep_interval / 2) * 100
-	var offset_reverse : int = keep_interval / 2 * 100
+	var offset := int((interval - keep_interval) * 100)
 	var last_word_deleted := ""
 	for i in range(tokens.size()-1, -1, -1):
 		# remove from here as they are positive
@@ -25,39 +26,35 @@ func merge_with_old_tokens(new_tokens: Array):
 			tokens = tokens.slice(0, i + 1)
 			last_word_deleted = tokens[i]["text"]
 			break
-	tokens_to_text(tokens)
-	tokens_to_text(new_tokens)
-	print(last_word_deleted)
-	for i in range(new_tokens.size()):
-		# remove from here as they are positive
-		if new_tokens[i]["t0"] - offset_reverse > 0:
-			new_tokens = new_tokens.slice(i)
-			break
-	tokens_to_text(new_tokens)
+	#tokens_to_text(tokens)
+	#tokens_to_text(new_tokens)
+	#tokens_to_text(new_tokens)
 	tokens.append_array(new_tokens)
 	
 
-func tokens_to_text(tokens):
+func tokens_to_text(speech_tokens):
 	text = ""
-	for token in tokens:
+	for token in speech_tokens:
 		text += token["text"]
 	print("Text: ... ", text, " ...")
 
-func _thread_function(buffer_full):
-	var new_tokens :Array= speech_to_text.transcribe(buffer_full)
+func _thread_function(buffer):
+	var new_tokens :Array= speech_to_text.transcribe(buffer)
 	new_tokens = new_tokens.filter(func (token): return !("[" in token["text"]) && !("<" in token["text"]))
 	if !tokens.is_empty():
 		merge_with_old_tokens(new_tokens)
 	else:
 		tokens = new_tokens
 	tokens_to_text(tokens)
+	#call_deferred()
+	#text_updated.emit(text)
 
 func _process(_delta):
 	var buffer: PackedVector2Array = effect_capture.get_buffer(effect_capture.get_frames_available())
 	buffer_full.append_array(buffer)
 	var mix_rate : int = ProjectSettings.get_setting("audio/driver/mix_rate")
-	var total_len : int = mix_rate * interval
-	var keep_len : int = mix_rate * keep_interval
+	var total_len := int(mix_rate * interval)
+	var keep_len := int(mix_rate * keep_interval)
 	if buffer_full.size() > total_len:
 		buffer_full.slice(buffer_full.size() - total_len)
 		var thread = Thread.new()
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
 #scons target=template_debug generate_bindings=no arch=arm64 dev_build=yes
-scons target=template_release generate_bindings=no arch=arm64
+scons target=template_release generate_bindings=no arch=arm64 precision=single
 rm -rf demo/addons
 cp -rf bin/addons demo/addons
 
