97ec0da05cddfa684b52f146eb648def2157445b
K. S. Ernest (iFire) Lee~ <ernest.lee@chibifire.com>
2023-12-02 05:01:40 -0800

    Run clang format.

--- src/speech_to_text.h
+++ src/speech_to_text.h
@@ -42,8 +42,9 @@ class SpeechToText : public Node {
 	Vector<whisper_token> prompt_tokens;
 	whisper_context *context_instance = nullptr;
 
-	float * buffer_float;
-	float * resampled_float;
+	float *buffer_float;
+	float *resampled_float;
+
 protected:
 	static void _bind_methods();
 
