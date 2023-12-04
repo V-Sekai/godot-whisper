533e202b490589599453d9477fc0c25cd4ae0eeb
Dragos Daian <daian.dragos@yahoo.com>
2023-12-04 16:31:16 +0100

    Add gif

--- demo/test_display.gd
+++ demo/test_display.gd
@@ -2,4 +2,12 @@ extends RichTextLabel
 
 
 func _on_speech_to_text_text_updated(total_time, p_text, new_text):
-	text = str(total_time) + ":" + p_text + "[color=orange]" + new_text + "[/color]"
+	transcribed = "Time to process: " + str(total_time) + "s\n" + p_text + "[color=orange]" + new_text + "[/color]"
+
+var transcribed := "..."
+
+func update_text():
+	text = "Time: " + str(Time.get_ticks_msec() / 1000) + "s\n" + transcribed
+
+func _process(delta):
+	update_text()
