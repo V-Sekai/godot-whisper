09f6ae52143b18ea75e8dae2c3bc61c636b543a5
Dragos Daian <daian.dragos@yahoo.com>
2024-03-13 18:27:59 +0100

    Move things to samples folder (#58)
    * move things to smaples folder
    
    * ship samples instead regularly with addon.
    
    * update samples dir from bin.
    
    * update scenes
    
    * update again

---
+++ bin/samples/godot_whisper/README.md
@@ -0,0 +1,5 @@
+Godot Whisper Sample
+
+Open either mic_transcribe.tscn or audio_transcribe.tscn
+Click on one StreamToText node. Download a language model.
+Either run the sample or run transcribe manually (for AudioStreamToText)
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
 scons target=template_release generate_bindings=no arch=universal precision=single
-rm -rf demo/addons/godot_whisper/bin
-cp -rf bin/addons/godot_whisper/bin demo/addons/godot_whisper/bin
+rm -rf samples/godot_whisper/addons/godot_whisper/bin
+cp -rf bin/addons/godot_whisper/bin samples/godot_whisper/addons/godot_whisper/bin
 
